e92fae8a7299982add2f4dcbd0317c0ceb2bdc1f
Sylwia Leung <sylwiokot@Sylwias-MBP.DWR-921E>
2020-08-19 18:42:37 +0200

    Added a custom error class and refactored a method

--- complexmath.py
+++ complexmath.py
@@ -12,13 +12,31 @@ import math
 import sys
 
 
+class MyExeption(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self._message = args[0]
+        else:
+            self._message = None
+
+    def __str__(self):
+        if self._message:
+            return self._message
+        else:
+            return "Cannot find a square root of a negative number or divide by 0."
+
+
 class ComplexMathOperations:
 
     @staticmethod
     def sqrt_of_sum_by_product(numbers: tuple) -> float:
-        if prod(numbers) <= 0:
-            raise ValueError('cannot find sqrt of a negative number or divide by 0')
-        return float("{:.3f}".format(math.sqrt(sum(numbers) / prod(numbers))))
+        if 0 in numbers:
+            raise MyExeption
+        operation = sum(numbers) / prod(numbers)
+        if operation <= 0:
+            raise MyExeption
+        return float("{:.3f}".format(math.sqrt(operation)))
 
     @staticmethod
     def median_multiply_add(numbers: tuple) -> float:
@@ -29,13 +47,13 @@ class ComplexMathOperations:
 
 class MyTestCase(unittest.TestCase):
     def test_sqrt(self):
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaises(MyExeption) as context:
             ComplexMathOperations.sqrt_of_sum_by_product((3, -5, 7, -8, 0, 9))
             ComplexMathOperations.sqrt_of_sum_by_product((3, -5, 7, 8, 4, 9))
 
-        self.assertTrue('cannot find sqrt of a negative number or divide by 0' in str(context.exception))
-        self.assertEqual(ComplexMathOperations.sqrt_of_sum_by_product((2, 3, -5, 7, -8, 9)), 0.023)
-        self.assertEqual(ComplexMathOperations.sqrt_of_sum_by_product(()), 0.0)
+        self.assertTrue('Cannot find a square root of a negative number or divide by 0.' in str(context.exception))
+        self.assertEqual(ComplexMathOperations.sqrt_of_sum_by_product((2, 1, -5, 7, -8, 9)), 0.035)
+        self.assertEqual(ComplexMathOperations.sqrt_of_sum_by_product((-9, -5, -10, 3)), 0.125)
 
     def test_median(self):
         with self.assertRaises(StatisticsError) as context:
@@ -49,3 +67,4 @@ class MyTestCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
