8f5c5f8774f3f186ae51b614ca14c59a2fdd28da
Patrycja Zając <patrycjaazajac@gmail.com>
2020-08-22 00:45:07 +0200

    refactor a code

--- geometry/geometryclass.py
+++ geometry/geometryclass.py
@@ -3,27 +3,27 @@ class Geometry:
     # Determining the midpoint of a segment
     @staticmethod
     def middle_segment(point1: tuple, point2: tuple) -> tuple:
-        (x1, y1) = point1   #coordinate of the point1
-        (x2,y2) = point2    #coordinate of the point2
-        A = (x1+x2)/2
-        B = (y1+y2)/2
+        (x1, y1) = point1  # coordinate of the point1
+        (x2, y2) = point2  # coordinate of the point2
+        A = (x1 + x2) / 2
+        B = (y1 + y2) / 2
         return (A, B)
 
     # Checks for a collision between two rectangles. Takes top left coordinate (l) and bottom right coordinate (r)
     @staticmethod
     def check_collision_between_rectangle(left_point1: tuple, right_point1: tuple,
-                                     left_point2: tuple, right_point2: tuple) -> bool:
+                                          left_point2: tuple, right_point2: tuple) -> bool:
 
-        x1, y1 = left_point1    #coordinate of the left_point1
-        x2, y2 = right_point1   #coordinate of the right_point1
-        x3, y3 = left_point2    #coordinate of the left_point2
-        x4, y4 = right_point2   #coordinate of the right_point2
+        x1, y1 = left_point1  # coordinate of the left_point1
+        x2, y2 = right_point1  # coordinate of the right_point1
+        x3, y3 = left_point2  # coordinate of the left_point2
+        x4, y4 = right_point2  # coordinate of the right_point2
 
-    # Checks if one rectangle is on left side of other
+        # Checks if one rectangle is on left side of other
         if x1 >= x4 or x3 >= x2:
             return False
 
-    # Checks if one rectangle is above other
+        # Checks if one rectangle is above other
         if y1 <= y4 or y3 <= y2:
             return False
 
--- test/geometryclass_test.py
+++ test/geometryclass_test.py
@@ -1,21 +1,22 @@
 import unittest
 from syl_pat_rep.geometry import geometryclass
 
+
 class MyTestCase(unittest.TestCase):
 
     def test_middle_segment_01(self):
         self.segment = geometryclass.Geometry()
-        middle = self.segment.middle_segment((3,2), (5,6))
-        self.assertEquals(middle, (4,4))
+        middle = self.segment.middle_segment((3, 2), (5, 6))
+        self.assertEquals(middle, (4, 4))
 
     def test_middle_segment_02(self):
         self.segment = geometryclass.Geometry()
-        middle = self.segment.middle_segment((-5,-2), (5,6))
-        self.assertEquals(middle, (0,2))
+        middle = self.segment.middle_segment((-5, -2), (5, 6))
+        self.assertEquals(middle, (0, 2))
 
     def test_check_collision_between_rectangle_01(self):
         self.rectangles = geometryclass.Geometry()
-        rectangles = self.rectangles.check_collision_between_rectangle((-3, 6), (-2, 0), (1,3), (3, 1))
+        rectangles = self.rectangles.check_collision_between_rectangle((-3, 6), (-2, 0), (1, 3), (3, 1))
         self.assertEquals(rectangles, False)
 
     def test_check_collision_between_rectangle_02(self):
